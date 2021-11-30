f73bda50811ee7526084eaacd8a955ab860e0479
Jorge Grimaldi <jgrimaldi@microsoft.com>
2021-11-30 16:21:25 -0600

    Adding cita

---
+++ Data/Models/Appointment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenSalud.Data.Models
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public string? HospitalName { get; set; }
+
+        public DateTime AppointmentDate { get; set; }
+
+        public string? DoctorName { get; set; }
+
+    }
+}
